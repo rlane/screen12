4a7e1e468b28fc7b28a7f632e2b9a16bc3289e91
Rich Lane <rlane@club.cc.cmu.edu>
2012-07-07 22:40:01 -0700

    add keys api

--- api.c
+++ api.c
@@ -28,6 +28,63 @@ static void sym_init(mrb_state *mrb)
 }
 
 
+/* SDL event handling */
+
+struct pressed_key {
+    bool valid;
+    SDL_keysym keysym;
+};
+
+#define MAX_PRESSED_KEYS 16
+struct pressed_key pressed_keys[MAX_PRESSED_KEYS];
+
+/* TODO dont miss short keypresses */
+static void handle_keyboard_event(const SDL_KeyboardEvent *event, bool down)
+{
+    if (event->keysym.sym == SDLK_ESCAPE) {
+        exit(0);
+    }
+
+    //fprintf(stderr, "key '%s' %s\n", SDL_GetKeyName(event->keysym.sym), down ? "down": "up");
+
+    int i;
+    for (i = 0; i < MAX_PRESSED_KEYS; i++) {
+        struct pressed_key *pk = &pressed_keys[i];
+        if (down) {
+            if (!pk->valid) {
+                pk->keysym = event->keysym;
+                pk->valid = true;
+                //fprintf(stderr, "added key '%s' at index %d\n", SDL_GetKeyName(pk->keysym.sym), i);
+                break;
+            } 
+        } else {
+            if (pk->valid && pk->keysym.sym == event->keysym.sym) {
+                pk->valid = false;
+                //fprintf(stderr, "removed key '%s' at index %d\n", SDL_GetKeyName(pk->keysym.sym), i);
+                /* Keep going to ensure unbalanced keydowns get cleaned up */
+            }
+        }
+    }
+}
+
+static void process_events(void)
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+            exit(0);
+            break;
+        case SDL_KEYDOWN:
+        case SDL_KEYUP:
+            handle_keyboard_event(&event.key, event.type == SDL_KEYDOWN);
+            break;
+        }
+    }
+}
+
+
 /* API functions */
 
 static mrb_value api_color(mrb_state *mrb, mrb_value self)
@@ -138,6 +195,23 @@ static mrb_value api_delay(mrb_state *mrb, mrb_value self)
     return mrb_nil_value();
 }
 
+static mrb_value api_keys(mrb_state *mrb, mrb_value self)
+{
+    process_events();
+    mrb_value keys = mrb_ary_new(mrb);
+
+    int i;
+    for (i = 0; i < MAX_PRESSED_KEYS; i++) {
+        struct pressed_key *pk = &pressed_keys[i];
+        if (!pk->valid) {
+            continue;
+        }
+        mrb_ary_push(mrb, keys, mrb_str_new_cstr(mrb, SDL_GetKeyName(pk->keysym.sym)));
+    }
+
+    return keys;
+}
+
 void api_init(mrb_state *mrb)
 {
     sym_init(mrb);
@@ -148,6 +222,7 @@ void api_init(mrb_state *mrb)
     mrb_define_method(mrb, mrb->kernel_module, "circle", api_circle, ARGS_REQ(3) | ARGS_OPT(1));
     mrb_define_method(mrb, mrb->kernel_module, "delay", api_delay, ARGS_REQ(1));
     mrb_define_method(mrb, mrb->kernel_module, "flip", api_flip, ARGS_NONE());
+    mrb_define_method(mrb, mrb->kernel_module, "keys", api_keys, ARGS_NONE());
 
     mrb_define_const(mrb, mrb->kernel_module, "SCREEN_WIDTH", mrb_fixnum_value(screen_width));
     mrb_define_const(mrb, mrb->kernel_module, "SCREEN_HEIGHT", mrb_fixnum_value(screen_height));
