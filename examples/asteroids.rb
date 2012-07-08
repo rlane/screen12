a0d4605dccbda4b7f14c557e3f7700d617c38507
Rich Lane <rlane@club.cc.cmu.edu>
2012-07-08 14:26:28 -0700

    asteroids: add bullets

--- examples/asteroids.rb
+++ examples/asteroids.rb
@@ -3,6 +3,7 @@
 # constants
 MAIN_ACC = 0.5
 ANGULAR_ACC = -0.005
+BULLET_SPEED = 5.0
 
 # create the player
 $player = {
@@ -10,6 +11,8 @@ $player = {
   vx: 0.0, vy: 0.0, angular_velocity: 0.0,
   main_acc: 0.0, angular_acc: 0.0
 }
+
+$bullets = []
 
 def handle_input
   $player[:main_acc] = 0.0
@@ -29,6 +32,14 @@ def handle_input
 
   if keys.member?('right')
     $player[:angular_acc] -= ANGULAR_ACC
+  end
+
+  if keys.member?('space')
+    angle = $player[:angle]
+    bvx = $player[:vx] + BULLET_SPEED * Math.cos(angle)
+    bvy = $player[:vy] + BULLET_SPEED * Math.sin(angle)
+    bullet = { x: $player[:x], y: $player[:y], vx: bvx, vy: bvy }
+    $bullets.push(bullet)
   end
 end
 
@@ -57,6 +68,29 @@ def move_player
   end
 end
 
+def move_bullets
+  $bullets.each do |bullet|
+    bullet[:x] += bullet[:vx]
+    bullet[:y] += bullet[:vy]
+
+    if bullet[:x] > SCREEN_WIDTH
+      bullet[:x] -= SCREEN_WIDTH
+    end
+
+    if bullet[:x] < 0
+      bullet[:x] += SCREEN_WIDTH
+    end
+
+    if bullet[:y] > SCREEN_HEIGHT
+      bullet[:y] -= SCREEN_HEIGHT
+    end
+
+    if bullet[:y] < 0
+      bullet[:y] += SCREEN_HEIGHT
+    end
+  end
+end
+
 def clear_screen
   color(0, 0, 0, 255)
   box(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, fill: true)
@@ -70,13 +104,22 @@ def draw_player
   polygon(coords, aa: true,
           position: [$player[:x], $player[:y]],
           rotation: $player[:angle])
-  flip
+end
+
+def draw_bullets
+  color(255, 255, 255, 200)
+  $bullets.each do |bullet|
+    line(bullet[:x], bullet[:y], bullet[:x], bullet[:y])
+  end
 end
 
 while true
   handle_input
   move_player
+  move_bullets
   clear_screen
   draw_player
+  draw_bullets
+  flip
   delay 15
 end
