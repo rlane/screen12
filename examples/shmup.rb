8be9d3107b8d66d5bee237646d4396c0b7a0b1f4
Rich Lane <rlane@club.cc.cmu.edu>
2012-07-10 23:48:18 -0700

    add another tyrian tileset

--- examples/shmup.rb
+++ examples/shmup.rb
@@ -1,13 +1,13 @@
-tileset = load_image("resources/sample.png", 0xBF, 0xDC, 0xBF)
+tyrian1 = load_image("resources/tyrian1.png", 0xBF, 0xDC, 0xBF)
+tyrian2 = load_image("resources/tyrian2.png", 0xBF, 0xDC, 0xBF)
 
 i = 0
 while true
   clear
   i += 1
   x = 400 + 300*(Math.cos(Math::PI*time/4000.0))
-  blit(tileset, 10, 10)
-  blit(tileset, x, 300, 48, 140, 24, 28)
-  blit(tileset, x, 400, 125, 154, 37, 28)
+  blit(tyrian1, x, 400, 125, 154, 37, 28)
+  blit(tyrian2, x, 100, 30, 2, 17, 25)
   display
   keys
   delay 30
