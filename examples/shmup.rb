74e28e5d2eda805c47ac5ef34d5bc3951700f2d7
Rich Lane <rlane@club.cc.cmu.edu>
2012-07-11 00:01:30 -0700

    add a higher level image api

--- examples/shmup.rb
+++ examples/shmup.rb
@@ -1,13 +1,10 @@
-tyrian1 = load_image("resources/tyrian1.png", 0xBF, 0xDC, 0xBF)
-tyrian2 = load_image("resources/tyrian2.png", 0xBF, 0xDC, 0xBF)
-
 i = 0
 while true
   clear
   i += 1
   x = 400 + 300*(Math.cos(Math::PI*time/4000.0))
-  blit(tyrian1, x, 400, 125, 154, 37, 28)
-  blit(tyrian2, x, 100, 30, 2, 17, 25)
+  image('tyrian/player', x, 400)
+  image('tyrian/enemy', x, 100)
   display
   keys
   delay 30
--- lib.rb
+++ lib.rb
@@ -19,3 +19,24 @@ $lib_prng = PRNG.new 1
 def random low, high
   $lib_prng.range low, high
 end
+
+COLORKEYS = {
+  "resources/tyrian1.png" => [0xBF, 0xDC, 0xBF],
+  "resources/tyrian2.png" => [0xBF, 0xDC, 0xBF],
+}
+
+IMAGES = {
+  # name => [filename, x, y, w, h]
+  'tyrian/player' => ["resources/tyrian1.png", 125, 154, 37, 28],
+  'tyrian/enemy' => ["resources/tyrian2.png", 30, 2, 17, 25],
+}
+
+IMAGE_CACHE = Hash.new { |h,k| h[k] = load_image(k, *(COLORKEYS[k] || [-1,-1,-1])) }
+
+def image name, x, y
+  raise "no such image: #{name}" unless IMAGES.member? name
+  filename, cx, cy, cw, ch = IMAGES[name]
+  surface = IMAGE_CACHE[filename]
+  blit surface, x, y, cx, cy, cw, ch
+  nil
+end
