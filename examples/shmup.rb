3998bcab411a829c6e6547eebc45f2fc5e82309b
Rich Lane <rlane@club.cc.cmu.edu>
2012-07-11 08:41:57 -0700

    shmup: randomly create enemies

--- examples/shmup.rb
+++ examples/shmup.rb
@@ -5,6 +5,24 @@ $player = {
   y: 500,
 }
 
+$enemies = []
+
+def create_enemy
+  enemy = {
+    x: random(100, SCREEN_WIDTH-100),
+    y: -30.0,
+    vx: 0.0,
+    vy: random(1.0, 3.0)
+  }
+  $enemies.push(enemy)
+end
+
+def randomly_create_enemies
+  if random(0, 1000) > 970
+    create_enemy
+  end
+end
+
 def handle_input
   if keys.member? 'left'
     $player[:x] -= PLAYER_SPEED
@@ -23,9 +41,18 @@ def handle_input
   end
 end
 
+def move_enemies
+  $enemies.each do |enemy|
+    enemy[:x] += enemy[:vx]
+    enemy[:y] += enemy[:vy]
+  end
+  $enemies = $enemies.select { |enemy| enemy[:y] < SCREEN_HEIGHT }
+end
+
 def draw_enemies
-  x = 400 + 300*(Math.cos(Math::PI*time/4000.0))
-  image('tyrian/enemy', x, 100)
+  $enemies.each do |enemy|
+    image('tyrian/enemy', enemy[:x], enemy[:y])
+  end
 end
 
 def draw_player
@@ -34,6 +61,8 @@ end
 
 while true
   handle_input
+  randomly_create_enemies
+  move_enemies
   clear
   draw_enemies
   draw_player
