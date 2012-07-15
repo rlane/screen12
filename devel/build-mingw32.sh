f37b4e8619a46869996e4d3dd32992c545b9ba87
Rich Lane <rlane@club.cc.cmu.edu>
2012-07-15 16:06:42 -0700

    copy all resources for mingw32 build

--- devel/build-mingw32.sh
+++ devel/build-mingw32.sh
@@ -12,7 +12,7 @@ cp screen12 screen12-win32/screen12.exe
 cp /usr/i486-mingw32/bin/{SDL.dll,SDL_gfx.dll,SDL_image.dll,SDL_mixer.dll,zlib1.dll,libpng15-15.dll} screen12-win32/
 cp lib.rb screen12-win32/
 mkdir screen12-win32/resources
-cp resources/tyrian*.png resources/LICENSE.tyrian screen12-win32/resources/
+cp `git ls-tree -r --name-only HEAD resources` screen12-win32/resources/
 mkdir screen12-win32/examples
 cp examples/*.rb screen12-win32/examples/
 
